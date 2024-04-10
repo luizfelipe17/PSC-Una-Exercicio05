b2663d983c9fd28bfad9e50359745a32ced8cb57
luizfelipe17 <luizfeliperabelo17@gmail.com>
2024-04-09 23:24:32 -0300

    Update Questao03.java

--- Questao03.java
+++ Questao03.java
@@ -9,7 +9,7 @@ Na opção 2: receber três notas e seus respectivos pesos, calcular e mostrar m
 Na opção 3: sair do programa.
 Verifique a possibilidade de opção inválida. Nesse caso, o programa deverá mostrar uma mensagem.
  */
-package psc.exercicio05;
+package com.mycompany.psc.exercicio05;
 
 import java.util.Scanner;
 
@@ -20,51 +20,110 @@ public class Questao03 {
         Scanner ler = new Scanner(System.in);
 
         int opcao;
+        double media1 = 0;
+        double media2 = 0;
+
+        outerLoop:
 
         while (true) {
 
-            System.out.print("1 - Média aritmética\n"
+            System.out.println("1 - Média aritmética\n"
                     + "2 - Média ponderada\n"
                     + "3 - Sair");
+            System.out.print("INFORME: ");
             opcao = ler.nextInt();
 
             if (opcao == 1 || opcao == 2) {
 
                 if (opcao == 1) {
-                    
-                   while(true){
-                       System.out.print("1 - Média aritmética\n");
-           
-                    System.out.println("Informe dois valores: ");
-                    int valor1 = ler.nextInt();
-                    int valor2 = ler.nextInt();
 
-                    double mediaAritmetica = (valor1 + valor2) / 2;
+                    while (true) {
+                        System.out.print("\n1 - Média aritmética\n");
 
-                    System.out.printf("Sua média aritmética é igual a: %.2f", mediaAritmetica);
-                    System.out.print(" 8 - Repetir     9 - Voltar     0 - Sair");
-                    opcao = ler.nextInt();
+                        System.out.println("Informe dois valores: ");
+                        double valor1 = ler.nextDouble();
+                        double valor2 = ler.nextDouble();
 
-                    if (opcao == 1) {
+                        double mediaAritmetica = (valor1 + valor2) / 2;
 
-                        break;
-
-                    } else if (opcao == 9) {
-
-                        break;
-
-                    } else {
-
-                        System.out.println("Informe um valor válido");
+                        System.out.printf("Sua média aritmética é igual a: %.2f", mediaAritmetica);
+                        System.out.println(" \n\n8 - Repetir     9 - Voltar     0 - Sair");
                         opcao = ler.nextInt();
 
+                        if (opcao == 8) {
+                            
+
+                        } else if (opcao == 9) {
+
+                            break;
+
+                        } else if (opcao == 0) {
+
+                            break outerLoop;
+
+                        } else {
+
+                            System.out.print("INFORME UM VALOR VÁLIDO");    
+                            break outerLoop;
+                            
+                        }
                     }
 
-                } else if (opcao = 2) {
+                } else if (opcao == 2) {
 
+                    while (true) {
+
+                        System.out.print("\n2 - Média ponderada\n");
+
+                        for (int i = 0; i < 3; i++) {
+
+                            System.out.println("Informe os valores e seus pesos: ");
+                            System.out.print("NOTA: ");
+                            double valor1 = ler.nextDouble();
+                            System.out.print("PESO: ");
+                            double valor2 = ler.nextDouble();
+
+                            double media = valor1 * valor2;
+                            media1 += media;
+                            media2 += valor2;
+
+                        }
+
+                        double mediaPonderada = media1 / media2;
+
+                        System.out.printf("A média ponderada é igual a: %.2f", mediaPonderada);
+                        System.out.println(" \n\n8 - Repetir     9 - Voltar     0 - Sair");
+                        opcao = ler.nextInt();
+
+                        if (opcao == 8) {
+
+                        } else if (opcao == 9) {
+
+                            break;
+
+                        } else if (opcao == 0) {
+
+                            break outerLoop;
+
+                        } else {
+
+                            System.out.print("INFORME UM VALOR VÁLIDO");    
+                            break outerLoop;
+
+                        }
+                    }
                 }
+
+            } else if (opcao == 3) {
+
+                System.out.print("ATÈ A PRÓXIMA!!!");
+                break;
+
+            } else {
+
+                    System.out.println("Informe um valor válido\n");
+
             }
         }
-
     }
 }
